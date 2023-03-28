1f6c0ca4bb29c3810eb9715ea12cb9fde6502486
Steven McIntyre <steven.mc2225@gmail.com>
2023-03-27 19:58:36 -0700

    added function for getting computer choice and cleaned up file

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,27 @@
-console.log("number 5 is alive!");
+// Rock Paper Scissors Game
 
-function game(rounds) {
-
+function getComputerChoice() { // Returns 'rock', 'paper', or 'scissors'.
+  let compChoice = Math.ceil(Math.random()*3);
+  switch (compChoice) {
+    case 1:
+      compChoice = "Rock";
+      break;
+    case 2:
+      compChoice = "Paper";
+      break;
+    case 3:
+      compChoice = "Scissors";
+      break;
+    default:
+      compChoice = "That is not a valid entry"
+      break;
+  }
+  return compChoice;
 }
 
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
